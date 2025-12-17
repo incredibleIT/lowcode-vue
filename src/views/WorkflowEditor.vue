498e05a3ef65e04fb27ab48fe82b00500c7a790e
yangyang1 <yangyang02@bamboocloud.cn>
2025-12-17 16:43:29 +0800

    20251217-重构工作流编辑器组件结构

--- src/views/WorkflowEditor.vue
+++ src/views/WorkflowEditor.vue
@@ -1,27 +1,5 @@
-<template>
-    <div class="main-workflow-editor">
-        <div class="c" @dragover="onDragOver" @drop="onDrop">
-            <VueFlow
-                :nodes="nodes"
-                :edges="edges"
-                :node-types="nodeTypes"
-                fit-view-on-init
-                @connect="handleConnect"
-            >
-                <Background pattern-color="#aaa" :gap="16"/>
-                <Controls/>
-            </VueFlow>
-        </div>
-
-        <div>
-            <RightsideBar/>
-        </div>
-
-    </div>
-</template>
-
 <script setup lang="ts">
-import {nextTick, ref} from "vue";
+import {isRef, nextTick, ref} from "vue";
 import {
     VueFlow,
     useVueFlow,
@@ -35,7 +13,6 @@ import {Background} from "@vue-flow/background"; // 背景组件
 import {Controls} from "@vue-flow/controls"; // 控制组件
 import "@vue-flow/core/dist/style.css";
 import {markRaw} from "vue";
-import {useRoute, useRouter} from "vue-router";
 import RightsideBar from "@/components/workflow/RightSidebar.vue";
 import CustomScriptNode from "@/components/nodes/CustomScriptNodes.vue";
 import HttpRequests from "@/components/nodes/HttpRequests.vue";
@@ -47,11 +24,8 @@ import PostgreSQLNode from "@/components/nodes/PostgreSQLNode.vue";
 import SplitterNode from "@/components/nodes/SplitterNode.vue";
 import CombinerNode from "@/components/nodes/CombinerNode.vue";
 
-const route = useRoute();
-const router = useRouter();
-
-const nodes = ref<Node[]>([])
-const edges = ref<Edge[]>([])
+// const nodes = ref<Node[]>([])
+// const edges = ref<Edge[]>([])
 
 const nodeTypes: NodeTypesObject = {
     customScript: markRaw(CustomScriptNode) as NodeComponent,
@@ -64,12 +38,7 @@ const nodeTypes: NodeTypesObject = {
     splitter: markRaw(SplitterNode) as NodeComponent,
     combiner: markRaw(CombinerNode) as NodeComponent,
 };
-
-const currentWorkflowName = ref("");
-
-const isExecuting = ref(false);
-const executionResult = ref(null);
-const {addNodes, addEdges, project} = useVueFlow();
+const {addNodes, addEdges, project, getConnectedEdges, getHandleConnections, getNodes, getEdges} = useVueFlow();
 
 const handleConnect = (connection: Connection) => {
     if (connection.source === connection.target) {
@@ -203,8 +172,28 @@ const onDrop = async (event: DragEvent) => {
     }
 };
 
-
 </script>
+
+<template>
+    <div class="main-workflow-editor">
+        <div class="c" @dragover="onDragOver" @drop="onDrop">
+            <VueFlow
+                :node-types="nodeTypes"
+                fit-view-on-init
+                @connect="handleConnect"
+            >
+                <Background pattern-color="#aaa" :gap="16"/>
+                <Controls/>
+            </VueFlow>
+        </div>
+
+        <div>
+            <RightsideBar/>
+        </div>
+
+    </div>
+</template>
+
 
 <style scoped>
 .main-workflow-editor {
